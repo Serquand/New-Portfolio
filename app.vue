7bcfa574a2bcea7268b7a2d54d22b521cd69d214
Serquand <esteban.vincent@efrei.net>
2025-02-16 01:37:05 +0100

    Add SeoMeta + 404 + Vercel.json

--- app.vue
+++ app.vue
@@ -1,5 +1,22 @@
+<script setup lang="ts">
+useSeoMeta({
+    title: 'Esteban Vincent - Portfolio',
+    ogTitle: 'Esteban Vincent - Portfolio',
+    description: 'Vous ne savez pas par où commencer votre projet Web ? Je vous guiderai à travers les étapes de conception de votre projet !',
+    ogType: 'website',
+    ogDescription: 'Vous ne savez pas par où commencer votre projet Web ? Je vous guiderai à travers les étapes de conception de votre projet !',
+    ogImage: 'https://serquand.com/S.png',
+    ogSiteName: 'Esteban Vincent - Portfolio',
+    ogLocale: 'fr_FR',
+    ogUrl: 'https://serquand.com/S.png',
+    robots: 'index, follow', // Permet l'indexation et le suivi des liens
+    canonicalUrl: 'https://serquand.com', // URL canonique de ton site
+    author: 'Esteban Vincent',
+});
+</script>
+
 <template>
-    <div>
-        <MainView />
-    </div>
+    <NuxtLayout>
+        <NuxtPage />
+    </NuxtLayout>
 </template>
---
+++ error.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="fixed inset-0 flex items-center justify-center bg-[#111]">
+        <h1 class="text-center flex items-center gap-10 text-3xl text-gray-200">
+            <div>404</div>
+            <div class="w-px h-18 bg-gray-500" />
+            <div>Page not found</div>
+        </h1>
+    </div>
+</template>
